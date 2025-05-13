917ca0df3e8be9e482e1e9dde152ce3a38c0b6f1
Spencer Cole <youremail@example.com>
2025-05-13 12:20:41 -0400

    Feature: added styled start screen with subtitle and button prompt

--- main.py
+++ main.py
@@ -3,6 +3,48 @@ import os
 import sys
 from game import Game
 
+def show_start_screen():
+    WIDTH, HEIGHT = 600, 400
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    BLUE = (50, 150, 255)
+
+    title_font = pygame.font.SysFont(None, 64)
+    subtitle_font = pygame.font.SysFont(None, 36)
+    start_font = pygame.font.SysFont(None, 28)
+
+    screen.fill(WHITE)
+
+    # Title
+    title_text = title_font.render("Dodge the Blocks", True, BLACK)
+    screen.blit(title_text, (WIDTH // 2 - title_text.get_width() // 2, 80))
+
+    # Subtitle
+    subtitle_text = subtitle_font.render("Avoid falling blocks. Survive as long as you can!", True, BLACK)
+    screen.blit(subtitle_text, (WIDTH // 2 - subtitle_text.get_width() // 2, 140))
+
+    # Start button box
+    box_width, box_height = 300, 50
+    box_x = WIDTH // 2 - box_width // 2
+    box_y = 250
+    pygame.draw.rect(screen, BLUE, (box_x, box_y, box_width, box_height), border_radius=10)
+
+    start_text = start_font.render("Press SPACE to Start", True, WHITE)
+    screen.blit(start_text, (WIDTH // 2 - start_text.get_width() // 2, box_y + 10))
+
+    pygame.display.flip()
+
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    waiting = False
+
 def run_game(high_score):
     WIDTH, HEIGHT = 600, 400
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -55,6 +97,7 @@ else:
 
 while True:
     try:
+        show_start_screen()
         final_score, high_score = run_game(high_score)
 
         # Save high score
